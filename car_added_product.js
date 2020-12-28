ac04ca51a2223f6511f2e57cd72d354c234ef647
Ashish Kumar <63970193+ashishk1506@users.noreply.github.com>
2020-12-28 15:38:40 +0530

    Add files via upload

---
+++ car_added_product.js
@@ -0,0 +1,168 @@
+function added0(){
+    document.getElementById("p0").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p0")
+    cart_button.style.backgroundColor= "green";
+  };
+  
+  function added1(){
+    document.getElementById("p1").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p1")
+    cart_button.style.backgroundColor= "green";
+  };
+  
+  function added2(){
+    document.getElementById("p2").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p2")
+    cart_button.style.backgroundColor= "green";
+  };
+  
+  function added3(){
+    document.getElementById("p3").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p3")
+    cart_button.style.backgroundColor= "green";
+  };
+  
+  function added4(){
+    document.getElementById("p4").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p4")
+    cart_button.style.backgroundColor= "green";
+  };
+  
+  function added5(){
+    document.getElementById("p5").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p5")
+    cart_button.style.backgroundColor= "green";
+  };
+  
+  function added6(){
+    document.getElementById("p6").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p6")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added7(){
+    document.getElementById("p7").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p7")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added8(){
+    document.getElementById("p8").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p8")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added9(){
+    document.getElementById("p9").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p9")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added10(){
+    document.getElementById("p10").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p10")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added11(){
+    document.getElementById("p11").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p11")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added12(){
+    document.getElementById("p12").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p12")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added13(){
+    document.getElementById("p13").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p13")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added14(){
+    document.getElementById("p14").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p14")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added15(){
+    document.getElementById("p15").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p15")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added16(){
+    document.getElementById("p16").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p16")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added17(){
+    document.getElementById("p17").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p17")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added18(){
+    document.getElementById("p18").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p18")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added19(){
+    document.getElementById("p19").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p19")
+    cart_button.style.backgroundColor= "green";
+  };
+
+   
+  function added20(){
+    document.getElementById("p20").innerHTML="Added!";
+  alert("You have added a product in your cart");
+    var cart_button = document.getElementById("p20")
+    cart_button.style.backgroundColor= "green";
+  };
+
+
+
+
+
+
+
+  
